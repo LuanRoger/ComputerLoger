971063570203644cd3c5d9e997df430fd0467872
LuanRoger <luan.roger.2003@gmail.com>
2020-07-10 18:42:43 -0300

    Comit inicial.

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerLoger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public FolderBrowserDialog logDir = new FolderBrowserDialog();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            logDir.ShowDialog();
+            SaveLog saveLog = new SaveLog();
+            saveLog.GenerateLog(logDir.SelectedPath);
+        }
+
+        private void btnSeeLog_Click(object sender, EventArgs e)
+        {
+            //Sistema Operacional
+            OsInfo osInfo = new OsInfo();
+            string logOsString = "===Sistema Operacional========================\n" +
+                $"{osInfo.SeeOsInfo()}\n";
+
+            //Processador
+            ProcessorInfo processorInfo = new ProcessorInfo();
+            string logProcessorString = "\n===Processador=============================\n" +
+                $"{processorInfo.SeeProcessorInfo()}\n";
+
+            //GPU
+            GpuInfo gpuInfo = new GpuInfo();
+            string logGpuString = "\n===GPU==================================\n" +
+                $"{gpuInfo.SeeGpuInfo()}";
+
+            MessageBox.Show(logOsString + logProcessorString + logGpuString, "Informações", MessageBoxButtons.OK);
+        }
+    }
+}
---
+++ GpuInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Management;
+
+class GpuInfo
+{
+    ManagementObjectSearcher gpuObjectSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController");
+    private string gpuName { get; set; }
+    private string gpuStatus { get; set; }
+    private string gpuTitle { get; set; }
+    private string gpuDisplayDrive { get; set; }
+    private string gpuDriveVersion { get; set; }
+    private string gpuVideoProcessor { get; set; }
+
+    public string SeeGpuInfo()
+    {
+        string gpuString = null;
+
+        foreach(ManagementObject gpuObject in gpuObjectSearcher.Get())
+        {
+            gpuName = gpuObject["Name"].ToString();
+            gpuStatus = gpuObject["Status"].ToString();
+            gpuTitle = gpuObject["Caption"].ToString();
+            gpuDisplayDrive = gpuObject["InstalledDisplayDrivers"].ToString();
+            gpuDriveVersion = gpuObject["DriverVersion"].ToString();
+            gpuVideoProcessor = gpuObject["VideoProcessor"].ToString();
+
+            gpuString = $"Nome: {gpuName}\nStatus: {gpuStatus}\nTitulo: {gpuTitle}\nDrive de vídeo: {gpuDisplayDrive}\n" +
+                $"Versão do drive: {gpuDriveVersion}\nProcessador de vídeo: {gpuVideoProcessor}";
+        }
+
+        return gpuString;
+    }
+}
+
